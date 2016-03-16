ad927206fc75b7cde765187d3b3d2adb3d2c4161
gabrielBusta <gabrielbusta@gmail.com>
2016-03-16 17:09:22 -0500

    Added stuff to the game win

--- Tfour_Main/Game.xaml.cs
+++ Tfour_Main/Game.xaml.cs
@@ -25,18 +25,71 @@ namespace Tfour_Main
         // We can use a reference to keep track of who's turn it is
         private PlayerInformation activePlayer;
         private const int ROWS = 6, COLS = 6;
-        //private Board board;
-
+        private int[,] board;
+        BitmapImage p1Token, p2Token; 
+         
         public Game()
         {
             InitializeComponent();
             Loaded += Game_Loaded;
+
+            turnsRemaining = 36;
+
+            board = new int[ROWS, COLS];
+
+            p1Token = new BitmapImage();
+            p1Token.BeginInit();
+            p1Token.UriSource = new Uri(".\\GreenStone.png");
+            p1Token.EndInit();
+
+            p2Token = new BitmapImage();
+            p2Token.BeginInit();
+            p2Token.UriSource = new Uri(".\\YellowStone.png");
+            p2Token.EndInit();
+
         }
 
         private void Game_Loaded(object sender, RoutedEventArgs e)
         {
             GameOptions go = new GameOptions();
             go.Show();
+        }
+
+        private void Cell_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            int row = (int)btn.GetValue(Grid.RowProperty);
+            int col = (int)btn.GetValue(Grid.ColumnProperty);
+
+            NextTurn();
+        }
+
+        private void NextTurn()
+        {
+            if (activePlayer == p1)
+                activePlayer = p2;
+            else
+                activePlayer = p1;
+
+            turnsRemaining--;
+
+            if (turnsRemaining == 0)
+            {
+                MessageBox.Show("Game Over");
+                ClearBoard();
+            }
+        }
+
+        private void ClearBoard()
+        {
+            for (int r = 0; r < ROWS; r++)
+            {
+                for (int c = 0; c < COLS; c++)
+                {
+                    
+                }
+            }
         }
     }
 }
